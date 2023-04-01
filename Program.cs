ede2b5e95b758fd7bfc53cd228a0d96d4cd620a9
Ramon Mesquita <ramonmesquitamacedo2222@gmail.com>
2023-04-01 11:36:53 -0300

    Exercício concluído

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("***Quanto deu o troco?***");
+
+Console.WriteLine();
+
+decimal dinheiro, ValorTotal, troco;
+
+        Console.Write("Por favor, digite o valor da compra: R$");
+        ValorTotal = Convert.ToDecimal(Console.ReadLine());
+
+                Console.WriteLine();
+
+                Console.Write("Por favor, digite o valor pago: R$");
+                dinheiro = Convert.ToDecimal(Console.ReadLine());
+
+                Console.WriteLine();
+
+                Console.Write("Valor do seu troco: ");
+                troco = dinheiro - ValorTotal;
+
+                Console.WriteLine($"{troco:C}");
+
+                Console.WriteLine();                
+                
+                Console.WriteLine("***Obrigado, e volte sempre.***");
+             
